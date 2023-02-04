798aafa38f8e83f259b20b68521c992ff40b3579
Cjponce <123854146+Cjponce@users.noreply.github.com>
2023-02-03 18:23:19 -0600

    Programa Funcionando
    ya el programa se corrio con sus funciones

--- TAREA_1_I_PARCIAL/TAREA_1_I_PARCIAL/Form1.Designer.cs
+++ TAREA_1_I_PARCIAL/TAREA_1_I_PARCIAL/Form1.Designer.cs
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.numero1textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // numero1textBox1
+            // 
+            this.numero1textBox1.Location = new System.Drawing.Point(208, 68);
+            this.numero1textBox1.Name = "numero1textBox1";
+            this.numero1textBox1.Size = new System.Drawing.Size(81, 20);
+            this.numero1textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(111, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingrese el numero";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(185, 107);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(387, 196);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numero1textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox numero1textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- TAREA_1_I_PARCIAL/TAREA_1_I_PARCIAL/Form1.cs
+++ TAREA_1_I_PARCIAL/TAREA_1_I_PARCIAL/Form1.cs
@@ -16,5 +16,50 @@ namespace TAREA_1_I_PARCIAL
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Decimal num1 = Convert.ToDecimal(numero1textBox1.Text);
+            string resultado = pos_nega(num1);
+            string par_imp = par_impar(num1);
+            MessageBox.Show("el numero es " + resultado + " y es " + par_imp);
+        }
+        private string par_impar(decimal num)
+        {
+            decimal parimpar = 0;
+            string res2 = " ";
+            decimal mayor = 0;
+
+            if (num > 0)
+            {
+                mayor = num;
+            }
+            else { mayor = num * -1; }
+            parimpar = mayor % 2;
+            if (parimpar > 0)
+            {
+                res2 = "impar";
+            }
+            else { res2 = "par"; }
+            return res2;
+        }
+        private string pos_nega(decimal num)
+        {
+            string res = " ";
+            if (num == 0)
+            {
+                res = "Neutro";
+            }
+            else if (num > 0)
+            {
+                res = "positivo";
+            }
+            else { res = "negativo"; }
+            return res;
+        }
+
+
+
+
     }
 }
